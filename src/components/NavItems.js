3cc8f35d76cf01aa8ddd01bad77e4e7be4b62762
Hossein Ahmadi <hossein.ahmadi98@outlook.com>
2019-03-09 18:39:29 +0330

    Navbar created

---
+++ src/components/NavItems.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {NavLink} from "react-router-dom";
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Icon from '@material-ui/core/Icon';
+
+const NavItems = () => (
+    <div className="right-dir">
+        <List>
+            <ListItem button component={NavLink} to="/">
+                <ListItemIcon><Icon>home</Icon></ListItemIcon>
+                <ListItemText classes={{primary:'font-applied'}} primary="داشبورد"/>
+            </ListItem>
+            <ListItem button component={NavLink} to="/profile">
+                <ListItemIcon><Icon>account_circle</Icon></ListItemIcon>
+                <ListItemText classes={{primary:'font-applied'}} primary="پروفایل"/>
+            </ListItem>
+        </List>
+    </div>
+);
+
+export default NavItems;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,55 @@
 import React from 'react';
-import {NavLink} from "react-router-dom";
+import Drawer from '@material-ui/core/Drawer';
+import Button from '@material-ui/core/Button';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import Icon from '@material-ui/core/Icon';
+import NavItems from "./NavItems";
 
-const Navbar = () => (
-    <div>
-        <NavLink to="/">Dashboard</NavLink>
-        <NavLink to="/profile">Profile</NavLink>
-    </div>
-);
 
-export default Navbar
+class Navbar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            right: false
+        }
+    };
+
+    toggleDrawer = (side, open) => () => {
+        this.setState({
+            [side]: open,
+        });
+    };
+
+    render() {
+        return (
+            <div className="right-dir">
+                <AppBar position="static">
+                    <Toolbar>
+                        <IconButton color="inherit" aria-label="Menu" onClick={this.toggleDrawer('right', true)} >
+                            <Icon>menu</Icon>
+                        </IconButton>
+                        <Typography variant="h6" color="inherit" className="grow font-applied">
+                            آفرتور
+                        </Typography>
+                        <Button color="inherit">Login</Button>
+                    </Toolbar>
+                </AppBar>
+                <Drawer anchor="right" open={this.state.right} onClose={this.toggleDrawer('right', false)}>
+                    <div
+                        tabIndex={0}
+                        role="button"
+                        onClick={this.toggleDrawer('right', false)}
+                        onKeyDown={this.toggleDrawer('right', false)}
+                    >
+                        <NavItems/>
+                    </div>
+                </Drawer>
+            </div>
+        );
+    }
+}
+
+export default Navbar;
