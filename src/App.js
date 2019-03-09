4053cf88b82836c597534253938a3e83bd9449cf
Hossein Ahmadi <hossein.ahmadi98@outlook.com>
2019-03-09 17:40:17 +0330

    cleaned project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.width: {window.innerWidth > 1000 ? 'large' : 'small'}
-          </p>
-          <span>تست فونت</span>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+       <p>Cleaned pri=oject</p>
       </div>
     );
   }
