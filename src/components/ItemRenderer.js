63be59e8e285f94bc3abf6b93cbc11efb0cab045
Hossein Ahmadi <hossein.ahmadi98@outlook.com>
2019-03-12 16:53:23 +0330

    Item renderer done

---
+++ src/components/ItemRenderer.js
@@ -0,0 +1,145 @@
+import React from 'react';
+import categories from '../data/categories';
+import flightCompanies from '../data/flightCompanies';
+import countries from '../data/countries';
+import cities from '../data/cities';
+import tripTypes from "../data/tripTypes";
+import flightClasses from "../data/flightClasses";
+import airports from "../data/airports";
+import busClasses from "../data/busClasses";
+import foodTypes from "../data/foodTypes";
+import busTerminals from "../data/busTerminals";
+import busCompanies from "../data/busCompanies";
+
+
+const ItemRenderer = (props) => {
+    let item = '';
+    switch (props.type) {
+        case 'category':
+            item = categories.find((item) => {
+                if (item.id === props.id) {
+                    return item.title;
+                } else {
+                    return undefined;
+                }
+            });
+            break;
+        case
+        'flightCompany':
+            item = flightCompanies.find((item) => {
+                if (item.id === props.id) {
+                    return item.title;
+                } else {
+                    return undefined;
+                }
+            })
+            ;
+            break;
+        case
+        'country':
+            item = countries.find((item) => {
+                if (item.id === props.id) {
+                    return item.title;
+                } else {
+                    return undefined;
+                }
+            })
+            ;
+            break;
+        case
+        'city':
+            item = cities.find((item) => {
+                if (item.id === props.id) {
+                    return item.title;
+                } else {
+                    return undefined;
+                }
+            })
+            ;
+            break;
+        case
+        'tripType':
+            item = tripTypes.find((item) => {
+                if (item.id === props.id) {
+                    return item.title;
+                } else {
+                    return undefined;
+                }
+            })
+            ;
+            break;
+        case
+        'flightClass':
+            item = flightClasses.find((item) => {
+                if (item.id === props.id) {
+                    return item.title;
+                } else {
+                    return undefined;
+                }
+            })
+            ;
+            break;
+        case
+        'airport':
+            item = airports.find((item) => {
+                if (item.id === props.id) {
+                    return item.title;
+                } else {
+                    return undefined;
+                }
+            })
+            ;
+            break;
+        case
+        'busClass':
+            item = busClasses.find((item) => {
+                if (item.id === props.id) {
+                    return item.title;
+                } else {
+                    return undefined;
+                }
+            })
+            ;
+            break;
+        case
+        'foodType':
+            item = foodTypes.find((item) => {
+                if (item.id === props.id) {
+                    return item.title;
+                } else {
+                    return undefined;
+                }
+            })
+            ;
+            break;
+        case
+        'busTerminal':
+            item = busTerminals.find((item) => {
+                if (item.id === props.id) {
+                    return item.title;
+                } else {
+                    return undefined;
+                }
+            })
+            ;
+            break;
+        case
+        'busCompany':
+            item = busCompanies.find((item) => {
+                if (item.id === props.id) {
+                    return item.title;
+                } else {
+                    return undefined;
+                }
+            })
+            ;
+            break;
+        default:
+            item = undefined;
+    }
+    return (
+        <span>{item.title}</span>
+    );
+};
+
+export default ItemRenderer;
--- src/components/TripItem.js
+++ src/components/TripItem.js
@@ -2,12 +2,13 @@ import React from 'react';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import Icon from '@material-ui/core/Icon';
+import ItemRenderer from "./ItemRenderer";
 
 const TripItem = (props) => (
     <TableRow>
         <TableCell align="center">{props.trip.tripType === 1 ? <Icon className="trip-icon">flight</Icon> : <Icon className="trip-icon">directions_bus</Icon>}</TableCell>
         <TableCell align="center">{props.trip.tripTitle}</TableCell>
-        <TableCell align="center">{props.trip.startCity}</TableCell>
+        <TableCell align="center">{<ItemRenderer id={props.trip.startCity} type="city"/>}</TableCell>
         <TableCell align="center">{props.trip.destinationCity}</TableCell>
         <TableCell align="center">{props.trip.tripDay}</TableCell>
         <TableCell align="center">{props.trip.tripTime}</TableCell>
