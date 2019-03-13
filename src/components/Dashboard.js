b374c5fb9cc5ca67a041190e8df345371a987d7f
Hossein Ahmadi <hossein.ahmadi98@outlook.com>
2019-03-13 17:51:07 +0330

    Dashboard grids done

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
 
 const Dashboard = () => (
-    <p>Dashboard</p>
+    <Grid container spacing={24} className="my-container">
+        <Grid item xs={12} sm={12} md={12} lg={6} xl={6}>
+            chart
+        </Grid>
+        <Grid item xs={12} sm={12} md={12} lg={3} xl={3}>
+            comments
+        </Grid>
+        <Grid item xs={12} sm={12} md={12} lg={3} xl={3}>
+            active plan
+        </Grid>
+    </Grid>
 );
 
 export default Dashboard;
