c2c3e38ae687699445ddffe88603184488fa9ba2
Hossein Ahmadi <hossein.ahmadi98@outlook.com>
2019-04-08 15:50:16 +0430

    Dashboard and profile animation added

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,21 +4,30 @@ import Comments from "./Comments";
 import ToursVisitsReport from "./ToursVisitsReport";
 import AgencyProfileVisitsReport from "./AgencyProfileVisitsReport";
 import AgencyCategoriesReport from "./AgencyCategoriesReport";
+import Slide from '@material-ui/core/Slide';
 
 
 const Dashboard = () => (
     <Grid container spacing={24} className="my-container">
         <Grid item xs={12} sm={12} md={12} lg={6} xl={6}>
-            <ToursVisitsReport/>
+            <Slide direction="down" timeout={{enter: '1000ms'}} in={true} mountOnEnter unmountOnExit>
+                <ToursVisitsReport/>
+            </Slide>
         </Grid>
         <Grid item xs={12} sm={12} md={12} lg={6} xl={6}>
-            <AgencyProfileVisitsReport/>
+            <Slide direction="left" timeout={{enter: '1000ms'}} in={true} mountOnEnter unmountOnExit>
+                <AgencyProfileVisitsReport/>
+            </Slide>
         </Grid>
         <Grid item xs={12} sm={12} md={12} lg={6} xl={6}>
-            <Comments/>
+            <Slide direction="right" timeout={{enter: '1000ms'}} in={true} mountOnEnter unmountOnExit>
+                <Comments/>
+            </Slide>
         </Grid>
         <Grid item xs={12} sm={12} md={12} lg={6} xl={6}>
-            <AgencyCategoriesReport/>
+            <Slide direction="up" timeout={{enter: '1000ms'}} in={true} mountOnEnter unmountOnExit>
+                <AgencyCategoriesReport/>
+            </Slide>
         </Grid>
     </Grid>
 );
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,15 +2,19 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import AgencyProfile from "./AgencyProfile";
 import UserProfile from "./UserProfile";
-
+import Slide from '@material-ui/core/Slide';
 
 const Profile = () => (
     <Grid container spacing={24} className="my-container">
         <Grid item lg={6} md={6} sm={12} xs={12}>
-            <AgencyProfile/>
+            <Slide direction="right" timeout={{enter: '1000ms'}} in={true} mountOnEnter unmountOnExit>
+                <AgencyProfile/>
+            </Slide>
         </Grid>
         <Grid item lg={6} md={6} sm={12} xs={12}>
-            <UserProfile/>
+            <Slide direction="left" timeout={{enter: '1000ms'}} in={true} mountOnEnter unmountOnExit>
+                <UserProfile/>
+            </Slide>
         </Grid>
     </Grid>
 );
