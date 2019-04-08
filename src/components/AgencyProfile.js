63826c88b976a27b2d3f876b066430f7c5d8a84b
Hossein Ahmadi <hossein.ahmadi98@outlook.com>
2019-04-08 15:22:19 +0430

    Agency profile updated

--- src/components/AgencyProfile.js
+++ src/components/AgencyProfile.js
@@ -5,12 +5,13 @@ import agency from '../data/agency';
 import Rate from "./Rate";
 import moment from 'moment-jalaali';
 import Button from "@material-ui/core/Button";
+import HeaderChip from "./HeaderChip";
 
 class AgencyProfile extends React.Component{
     render() {
         return(
             <Paper elevation={1} className="right-dir agency-paper">
-                <h3 className="font-applied">اطلاعات آژانس</h3>
+                <HeaderChip label="اطلاعات آژانس" color="#4527a0" icon="account_balance"/>
                 <Divider/>
                 <br/>
                 <img className="agency-logo" alt={agency.name} src={agency.logo} />
@@ -20,7 +21,7 @@ class AgencyProfile extends React.Component{
                 <p><span className="bold">تلفن ثابت: </span>{agency.phone}</p>
                 <p><span className="bold">تلفن همراه: </span>{agency.mobile}</p>
                 <p><span className="bold">حوزه فعالیت: </span>{agency.activityArea}</p>
-                <p><span className="bold">تاریخ تاسیس: </span>{moment().format('jYYYY/jMM/jDD')}</p>
+                <p><span className="bold">تاریخ تاسیس: </span>{moment().subtract(6,'years').format('jYYYY/jMM/jDD')}</p>
                 <p><Rate rate={agency.rate}/></p>
                 <Button variant="contained" color="primary" className="edit-button">ویرایش</Button>
             </Paper>
