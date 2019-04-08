e496351c6ee342cd6c2a34473604b980213b55a8
Hossein Ahmadi <hossein.ahmadi98@outlook.com>
2019-04-08 15:19:36 +0430

    User profile updated

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -3,12 +3,14 @@ import Paper from '@material-ui/core/Paper';
 import Divider from '@material-ui/core/Divider';
 import user from '../data/user';
 import Button from "@material-ui/core/Button";
+import moment from 'moment-jalaali';
+import HeaderChip from "./HeaderChip";
 
 class UserProfile extends React.Component{
     render(){
         return(
             <Paper elevation={1} className="right-dir agency-paper">
-                <h3 className="font-applied">اطلاعات کاربر</h3>
+                <HeaderChip label='اطلاعات کاربری' color='#00838f' icon='account_circle'/>
                 <Divider/>
                 <br/>
                 <img className="user-image" alt={user.username} src={user.image} />
@@ -16,6 +18,8 @@ class UserProfile extends React.Component{
                 <p><span className="bold">نام خانوادگی: </span>{user.familyName}</p>
                 <p><span className="bold">نام کاربری: </span>{user.username}</p>
                 <p><span className="bold">تلفن همراه: </span>{user.phone}</p>
+                <p><span className="bold">تاریخ تولد: </span>{moment().subtract(25,'years').format('jDD jMMMM jYYYY')}</p>
+                <p><span className="bold">تاریخ ثبت نام: </span>{moment().subtract(3,'months').format('jDD jMMMM jYYYY')}</p>
                 <Button variant="contained" color="primary" className="edit-button">ویرایش</Button>
             </Paper>
         );
