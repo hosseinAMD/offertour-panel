beeeb80799d891e3644b89687b805e49be0ed4dd
Hossein Ahmadi <hossein.ahmadi98@outlook.com>
2019-04-09 16:17:07 +0430

    NavbarDateTime changed

--- src/components/NavbarTimeDate.js
+++ src/components/NavbarTimeDate.js
@@ -1,23 +1,13 @@
 import React from 'react';
 import moment from 'moment-jalaali';
 
-class NavbarTimeDate extends React.Component{
-    state={
-        now:''
-    };
 
-    render() {
-        setInterval(() => {
-            const now = moment();
-            this.setState(() => ({now}));
-        },60000);
-        return (
-            <div className="navbar-time-date">
-                <span className="right-dir font-applied">{moment().format('jDD jMMMM jYYYY')}</span>
-                <span>{moment().format('HH:mm')}</span>
-            </div>
-        );
-    }
-}
+const NavbarTimeDate = () => (
+    <div className="navbar-time-date">
+        <span className="right-dir font-applied">{moment().format('jDD jMMMM jYYYY')}</span>
+        <span className="right-dir font-applied">{moment().format('DD MMMM YYYY')}</span>
+    </div>
+);
+
 
 export default NavbarTimeDate;
