6c7cc16cac1c013c8616bebfc2df8a8ca64967dc
Hossein Ahmadi <hossein.ahmadi98@outlook.com>
2019-04-10 17:48:01 +0430

    Added province to data

--- src/data/provinces.js
+++ src/data/provinces.js
@@ -1,64 +1,64 @@
-const cities = [
+const provinces = [
     {
         id:1,
         title:'تهران',
-        province:1
+        country:1
     },
     {
         id:2,
-        title:'کیش',
-        province:2
+        title:'هرمزگان',
+        country:1
     },
     {
         id:3,
         title:'اصفهان',
-        province:3
+        country:1
     },
     {
         id:4,
-        title:'دبی',
-        province:4
+        title:'استان دبی',
+        country:4
     },
     {
         id:5,
-        title:'ابوظبی',
-        province:5
+        title:'استان ابوظبی',
+        country:4
     },
     {
         id:6,
         title:'پاریس',
-        province:6
+        country:7
     },
     {
         id:7,
-        title:'لیون',
-        province:7
+        title:'لیونس',
+        country:7
     },
     {
         id:8,
-        title:'ایروان',
-        province:8
+        title:'ارمنیا',
+        country:10
     },
     {
         id:9,
         title:'آنتالیا',
-        province:9
+        country:9
     },
     {
         id:10,
         title:'استانبول',
-        province:10
+        country:9
     },
     {
         id:11,
         title:'آنکارا',
-        province:11
+        country:9
     },
     {
         id:12,
-        title:'دبی',
-        province:12
-    }
+        title:'استان دبی',
+        country:11
+    },
 ];
 
-export default cities;
+export default provinces;
