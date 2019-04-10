bcc7a56138a494df63157c4ced570e1add31b45b
Hossein Ahmadi <hossein.ahmadi98@outlook.com>
2019-04-10 18:40:58 +0430

    Add airport designed

--- src/components/AirportsSetting.js
+++ src/components/AirportsSetting.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Slide from '@material-ui/core/Slide';
+import AddCity from "./AddCity";
+import FilterCity from "./FilterCity";
+import CitiesList from "./CitiesList";
+import AddAirport from "./AddAirport";
 
 const AirportsSetting = () => (
-    <div>
-        Airport Settings
-    </div>
+    <Grid container spacing={24} className="my-container">
+        <Grid item lg={6} md={6} sm={12} xs={12}>
+            <Slide direction="right" in={true} mountOnEnter>
+                <FilterCity/>
+            </Slide>
+        </Grid>
+        <Grid item lg={6} md={6} sm={12} xs={12}>
+            <Slide direction="left" in={true} mountOnEnter>
+                <AddAirport/>
+            </Slide>
+        </Grid>
+        <Grid item lg={12} md={12} sm={12} xs={12}>
+            <Slide direction="up" in={true} mountOnEnter>
+                <CitiesList/>
+            </Slide>
+        </Grid>
+    </Grid>
 );
 
 export default AirportsSetting;
