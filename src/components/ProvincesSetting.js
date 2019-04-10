e36816b5de219fb30e5b61a536c132f7f59ee61e
Hossein Ahmadi <hossein.ahmadi98@outlook.com>
2019-04-10 17:56:07 +0430

    Add Province done

--- src/components/ProvincesSetting.js
+++ src/components/ProvincesSetting.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Slide from '@material-ui/core/Slide';
+import AddProvince from "./AddProvince";
 
 const ProvincesSetting = () => (
-    <div>
-        provinces setting
-    </div>
+    <Grid container spacing={24} className="my-container">
+        <Grid item lg={6} md={6} sm={12} xs={12}>
+            <Slide direction="right" in={true} mountOnEnter>
+                <div>province filte</div>
+            </Slide>
+        </Grid>
+        <Grid item lg={6} md={6} sm={12} xs={12}>
+            <Slide direction="left" in={true} mountOnEnter>
+                <AddProvince/>
+            </Slide>
+        </Grid>
+        <Grid item lg={12} md={12} sm={12} xs={12}>
+            <Slide direction="up" in={true} mountOnEnter>
+                <div>province list</div>
+            </Slide>
+        </Grid>
+    </Grid>
 );
 
 export default ProvincesSetting;
