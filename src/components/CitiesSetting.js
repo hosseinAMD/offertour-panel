18c85ed85f2ed46142c24a2df2778d7249c8c5f8
Hossein Ahmadi <hossein.ahmadi98@outlook.com>
2019-04-10 18:18:20 +0430

    Add city designed

--- src/components/CitiesSetting.js
+++ src/components/CitiesSetting.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Slide from '@material-ui/core/Slide';
+import AddProvince from "./AddProvince";
+import FilterProvince from "./FilterProvince";
+import ProvincesList from "./ProvincesList";
+import AddCity from "./AddCity";
 
 const CitiesSetting = () => (
-    <div>
-        cities setting
-    </div>
+    <Grid container spacing={24} className="my-container">
+        <Grid item lg={6} md={6} sm={12} xs={12}>
+            <Slide direction="right" in={true} mountOnEnter>
+                <FilterProvince/>
+            </Slide>
+        </Grid>
+        <Grid item lg={6} md={6} sm={12} xs={12}>
+            <Slide direction="left" in={true} mountOnEnter>
+                <AddCity/>
+            </Slide>
+        </Grid>
+        <Grid item lg={12} md={12} sm={12} xs={12}>
+            <Slide direction="up" in={true} mountOnEnter>
+                <ProvincesList/>
+            </Slide>
+        </Grid>
+    </Grid>
 );
 
 export default CitiesSetting;
