b5c2be95e271146e23cf0f55e0d7854e981875da
Hossein Ahmadi <hossein.ahmadi98@outlook.com>
2019-04-25 02:05:52 +0430

    config variables bug fixed

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,7 @@
 const baseUrl = 'http://148.251.173.46:1010/api/v1';
 export const token = JSON.parse(localStorage.getItem('user')).data.token;
-export const loggedInUser = JSON.parse(localStorage.getItem('user')).data.information;
 export const role = localStorage.getItem('role');
+export const loggedInUser = role === 'support' ?
+    JSON.parse(localStorage.getItem('user')).data.information :
+    JSON.parse(localStorage.getItem('user')).data;
 export default baseUrl;
