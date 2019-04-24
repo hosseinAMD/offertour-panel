d2bf821e785b977a10d5b68515b6e61d012ad5dd
Hossein Ahmadi <hossein.ahmadi98@outlook.com>
2019-04-25 01:31:45 +0430

    Add agency done

--- src/components/AgencyForm.js
+++ src/components/AgencyForm.js
@@ -4,29 +4,37 @@ import {DatePicker} from 'react-advance-jalaali-datepicker';
 import TextField from "@material-ui/core/TextField";
 import FormControl from "@material-ui/core/FormControl";
 import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 import axios from 'axios';
 import baseUrl, {token} from '../config/config';
 import InputAdornment from "@material-ui/core/InputAdornment";
 import MenuItem from '@material-ui/core/MenuItem';
 import Icon from '@material-ui/core/Icon';
-import provinces from "../data/provinces";
-import cities from "../data/cities";
+import {connect} from "react-redux";
 
 class AgencyForm extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            OwnerName:  '',
-            OwnerFamilyName:  '',
+            OwnerName: '',
+            OwnerFamilyName: '',
             Image: '',
             Name: '',
-            Address:'',
-            EstabilishedDate:  '',
-            ProvinceID:0,
-            CityID:  '',
+            Address: '',
+            EstabilishedDate: '',
+            ProvinceID: 0,
+            CityID: '',
+            open: false
         };
     }
 
+    handleClose = () => {
+        this.setState({open: false});
+    };
 
     DatePickerInput = (props) => {
         return <input className="popo" {...props} ></input>;
@@ -50,20 +58,18 @@ class AgencyForm extends React.Component {
     handleSubmit = () => {
         let agencyUserDetail = new FormData();
         agencyUserDetail.append('Name', this.state.Name);
-        agencyUserDetail.append('FamilyName', this.state.FamilyName);
-        agencyUserDetail.append('PhoneNumber', this.state.PhoneNumber);
-        agencyUserDetail.append('UserName', this.state.UserName);
-        agencyUserDetail.append('Password', this.state.Password);
+        agencyUserDetail.append('OwnerName', this.state.OwnerName);
+        agencyUserDetail.append('OwnerFamilyName', this.state.OwnerFamilyName);
+        agencyUserDetail.append('EstabilishedDate', this.state.EstabilishedDate);
+        agencyUserDetail.append('CityID', this.state.CityID);
+        agencyUserDetail.append('Address', this.state.Address);
         agencyUserDetail.append('Image', this.state.Image);
-        agencyUserDetail.append('RoleID', this.state.RoleID);
-        agencyUserDetail.append('RegistrationDate', this.state.RegistrationDate);
-        agencyUserDetail.append('BirthDate', this.state.BirthDate);
-        axios.post(baseUrl + '/Agency/AgencyUser', agencyUserDetail, {
+        axios.post(baseUrl + '/Admin/Agency', agencyUserDetail, {
             headers: {
                 'Content-Type': 'application/json',
                 'token': token
             }
-        }).then(res => alert('done' + res)).catch(err => alert('error' + err));
+        }).then(res => this.setState(() => ({open: true}))).catch(err => alert('error' + err));
     };
 
 
@@ -85,7 +91,7 @@ class AgencyForm extends React.Component {
                     />
                     <TextField
                         id="OwnerName"
-                        label="نام مالک آژنس"
+                        label="نام مالک آژانس"
                         InputLabelProps={{className: 'input-labels'}}
                         InputProps={{className: 'font-applied'}}
                         FormHelperTextProps={{className: 'font-applied'}}
@@ -133,11 +139,11 @@ class AgencyForm extends React.Component {
                             startAdornment: <InputAdornment position="start"><Icon>golf_course</Icon></InputAdornment>,
                         }}
                     >
-                        {provinces.map(option => {
-                            if (option.country === 1) {
+                        {this.props.provinces.map(option => {
+                            if (option.CountryID === 1) {
                                 return (
-                                    <MenuItem className="font-applied" key={option.id} value={option.id}>
-                                        {option.title}
+                                    <MenuItem className="font-applied" key={option.Id} value={option.Id}>
+                                        {option.Name}
                                     </MenuItem>
                                 );
                             } else {
@@ -157,11 +163,11 @@ class AgencyForm extends React.Component {
                             startAdornment: <InputAdornment position="start"><Icon>location_on</Icon></InputAdornment>,
                         }}
                     >
-                        {cities.map(option => {
-                            if (option.province === this.state.ProvinceID) {
+                        {this.props.cities.map(option => {
+                            if (option.ProvinceID === this.state.ProvinceID) {
                                 return (
-                                    <MenuItem className="font-applied" key={option.id} value={option.id}>
-                                        {option.title}
+                                    <MenuItem className="font-applied" key={option.Id} value={option.Id}>
+                                        {option.Name}
                                     </MenuItem>
                                 );
                             } else {
@@ -189,9 +195,33 @@ class AgencyForm extends React.Component {
                     <Button onClick={this.props.agencyUser ? this.handleEdit : this.handleSubmit} variant="contained"
                             color="primary" className="edit-button">افزودن</Button>
                 </div>
+                <Dialog
+                    open={this.state.open}
+                    onClose={this.handleClose}
+                    className="right-dir font-applied"
+                >
+                    <DialogTitle id="alert-dialog-title" className="font-applied"> <Icon style={{color: 'green'}}
+                                                                                         fontSize="large">check_circle</Icon>{"انجام شد!"}
+                    </DialogTitle>
+                    <DialogContent>
+                        <DialogContentText id="alert-dialog-description">
+                            آژانس مورد نظر با موفقیت ایجاد شد!
+                        </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button className="font-applied" onClick={this.handleClose} color="primary" autoFocus>
+                            بستن
+                        </Button>
+                    </DialogActions>
+                </Dialog>
             </div>
         );
     }
 }
 
-export default AgencyForm;
+const mapStateToProps = (state) => ({
+    provinces: state.provinces,
+    cities: state.cities
+});
+
+export default connect(mapStateToProps)(AgencyForm);
