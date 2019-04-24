2fdc86ffe04915a62f0ffe1e15070a4a0b457c91
Hossein Ahmadi <hossein.ahmadi98@outlook.com>
2019-04-24 20:26:24 +0430

    store of redux done

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,21 @@
 import {createStore, combineReducers} from 'redux';
 import categoryReducer from "../reducers/categories";
+import countryReducer from "../reducers/countries";
+import provinceReducer from "../reducers/provinces";
+import cityReducer from "../reducers/cities";
+import airportReducer from "../reducers/airports";
+import terminalReducer from "../reducers/terminals";
 
 
 export default () => {
     const store = createStore(
         combineReducers({
             categories: categoryReducer,
+            countries: countryReducer,
+            provinces: provinceReducer,
+            cities: cityReducer,
+            airports: airportReducer,
+            terminals: terminalReducer,
         }),
         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     );
