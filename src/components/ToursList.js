b5135e4d7790298c4a8dacd809bc6dc1a5f1932c
Hossein Ahmadi <hossein.ahmadi98@outlook.com>
2019-04-26 00:02:14 +0430

    get agency active tours api fetched

--- src/components/ToursList.js
+++ src/components/ToursList.js
@@ -7,32 +7,64 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import tours from '../data/tours';
 import TourItem from "./TourItem";
+import axios from 'axios';
+import baseUrl, {token} from "../config/config";
+import Loading from "./Loading";
 
-const ToursList = () => (
-    <Grid container spacing={24} className="my-container">
-        <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-            <Table className="font-applied right-dir">
-                <TableHead>
-                    <TableRow>
-                        <TableCell align="center">کد تور</TableCell>
-                        <TableCell align="center">عنوان تور</TableCell>
-                        <TableCell align="center">کشور تور</TableCell>
-                        <TableCell align="center">قیمت</TableCell>
-                        <TableCell align="center">تاریخ شروع</TableCell>
-                        <TableCell align="center">تاریخ پایان</TableCell>
-                        <TableCell align="center">مدت</TableCell>
-                        <TableCell align="center">وضعیت نمایش</TableCell>
-                        <TableCell align="center">نوع تور</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {tours.map((tour) => (
-                        <TourItem key={tour.id} tour={tour}/>
-                    ))}
-                </TableBody>
-            </Table>
-        </Grid>
-    </Grid>
-);
+class ToursList extends React.Component {
+    state = {
+        tours: [],
+        isLoaded: false
+    };
+
+    componentDidMount() {
+        axios.get(baseUrl + '/Agency/Tour', {
+            headers: {
+                'token': token
+            }
+        }).then(res => this.setState(() => ({tours: res.data, isLoaded: true})))
+            .catch(err => alert(err))
+    }
+
+    render() {
+        if (this.state.isLoaded) {
+            if (this.state.tours.length > 0) {
+                return (
+                    <Grid container spacing={24} className="my-container">
+                        <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                            <Table className="font-applied right-dir">
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell align="center">کد تور</TableCell>
+                                        <TableCell align="center">عنوان تور</TableCell>
+                                        <TableCell align="center">کشور تور</TableCell>
+                                        <TableCell align="center">قیمت</TableCell>
+                                        <TableCell align="center">تاریخ شروع</TableCell>
+                                        <TableCell align="center">تاریخ پایان</TableCell>
+                                        <TableCell align="center">مدت</TableCell>
+                                        <TableCell align="center">وضعیت نمایش</TableCell>
+                                        <TableCell align="center">نوع تور</TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    {this.state.tours.map((tour) => (
+                                        <TourItem key={tour.id} tour={tour}/>
+                                    ))}
+                                </TableBody>
+                            </Table>
+                        </Grid>
+                    </Grid>
+                );
+            } else {
+                return (
+                    <p className="font-applied center-txt right-dir">در حال حاضر تور فعالی موجود نمی باشد. در صورتی که توری ثبت کرده اید منتظر
+                        بمانید تا پشتیبان آفرتور تور شما را بررسی و تایید کند.</p>
+                );
+            }
+        } else {
+            return (<Loading/>);
+        }
+    }
+}
 
 export default ToursList;
