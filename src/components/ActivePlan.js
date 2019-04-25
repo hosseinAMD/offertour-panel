fca743dc7536fa3e54f83c4d61ce854a0d8e2d6e
Hossein Ahmadi <hossein.ahmadi98@outlook.com>
2019-04-25 15:13:04 +0430

    active plans template done

--- src/components/ActivePlan.js
+++ src/components/ActivePlan.js
@@ -8,30 +8,57 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import HeaderChip from "./HeaderChip";
 import ActivePlanItem from "./ActivePlanItem";
-
+import axios from 'axios';
+import baseUrl, {token} from "../config/config";
+import Loading from "./Loading";
 
 class ActivePlan extends React.Component {
+    state = {
+        plans: '',
+        isLoaded: false
+    };
+
+
+    componentDidMount() {
+        axios.get(baseUrl + '/Agency/ActivePlan', {
+            headers: {
+                'Content-Type': 'application/json',
+                'token': token
+            }
+        }).then(res => this.setState(() => ({plans: res.data, isLoaded: true})))
+    }
+
     render() {
-        return (
-            <Paper elevation={1} className="right-dir plan-paper">
-                <HeaderChip label='اطلاعات پلن فعال' color='#0288d1' icon='attach_money'/>
-                <Divider/>
-                <br/>
-                <Table className="font-applied">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell align="center">عنوان</TableCell>
-                            <TableCell align="center">اعتبار</TableCell>
-                            <TableCell align="center">تور معمولی</TableCell>
-                            <TableCell align="center">تور ویژه</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        <ActivePlanItem/>
-                    </TableBody>
-                </Table>
-            </Paper>
-        );
+        if (this.state.isLoaded) {
+            if (this.state.plans) {
+                return (
+                    <Paper elevation={1} className="right-dir plan-paper">
+                        <HeaderChip label='اطلاعات پلن فعال' color='#0288d1' icon='attach_money'/>
+                        <Divider/>
+                        <br/>
+                        <Table className="font-applied">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell align="center">عنوان</TableCell>
+                                    <TableCell align="center">اعتبار</TableCell>
+                                    <TableCell align="center">تور معمولی</TableCell>
+                                    <TableCell align="center">تور ویژه</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {this.state.plans.map((item) => (
+                                    item.Status ? <ActivePlanItem plan={item}/> : ''
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </Paper>
+                );
+            } else {
+                return (<p>در حال حاضر پلن فعالی وجود نیست</p>)
+            }
+        } else {
+            return (<Loading/>)
+        }
     }
 }
 
--- src/components/ActivePlanItem.js
+++ src/components/ActivePlanItem.js
@@ -3,10 +3,11 @@ import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
 import DurationBadge from "./DurationBadge";
 import CountBadge from "./CountBadge";
+import ItemRenderer from "./ItemRenderer";
 
 const ActivePlanItem = (props) => (
     <TableRow>
-        <TableCell align="center">۲ ستاره</TableCell>
+        <TableCell align="center"><ItemRenderer type="plan" id={props.plan.PlanID}/></TableCell>
         <TableCell align="center"><DurationBadge duration={12}/></TableCell>
         <TableCell align="center"><CountBadge count={7}/></TableCell>
         <TableCell align="center"><CountBadge count={3}/></TableCell>
