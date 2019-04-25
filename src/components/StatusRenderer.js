24d367dbb2d4a7e5d590668c658f67350515cd58
Hossein Ahmadi <hossein.ahmadi98@outlook.com>
2019-04-26 03:34:14 +0430

    status renderer bug fixed

--- src/components/StatusRenderer.js
+++ src/components/StatusRenderer.js
@@ -3,7 +3,7 @@ import Icon from '@material-ui/core/Icon';
 import Fab from '@material-ui/core/Fab';
 
 const StatusRenderer = (props) => {
-    if (props.status === 1) {
+    if (props.status === 1 || props.status === true) {
         return (
             <Fab size="small" color="secondary" aria-label="enable" className="enable-status">
                 <Icon>done</Icon>
