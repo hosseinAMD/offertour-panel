2868e6cf8194eefa9ef94d3583f7086ec4b14fe3
Hossein Ahmadi <hossein.ahmadi98@outlook.com>
2019-04-26 03:24:27 +0430

    accept and reject style done

--- src/components/TourDetail.js
+++ src/components/TourDetail.js
@@ -4,23 +4,41 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Icon from '@material-ui/core/Icon';
 import Paper from '@material-ui/core/Paper';
-import tours from '../data/tours';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 import TourInfo from "./TourInfo";
 import TourTimeline from "./TourTimeline";
 import TourHotels from "./TourHotels";
 import TourDesc from "./TourDesc";
 import {connect} from "react-redux";
+import {NavLink} from "react-router-dom";
+import axios from 'axios';
+import baseUrl,{token} from "../config/config";
 
 class TourDetail extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            value: 0
+            value: 0,
+            openAccept: false,
+            openReject: false,
         }
     }
 
     handleChange = (event, value) => {
         this.setState({value});
+    };
+
+    handleAccept = () => {
+      this.setState(() => ({openAccept: true}))
+    };
+
+    handleReject = () => {
+        this.setState(() => ({openReject: true}))
     };
 
     render() {
@@ -46,6 +64,52 @@ class TourDetail extends React.Component {
                         {this.state.value === 1 ? <TourTimeline id={tour.Id}/> : ''}
                         {this.state.value === 2 ? <TourHotels id={tour.Id}/> : ''}
                         {this.state.value === 3 ? <TourDesc tour={tour}/> : ''}
+                        <div className="tour-handlers">
+                            <Button onClick={this.handleAccept} variant="contained" color="primary"
+                                    className="edit-button">تایید تور</Button>
+                            <Button onClick={this.handleReject} variant="contained" color="secondary" className="reject-button">حذف تور</Button>
+                        </div>
+                        <Dialog
+                            open={this.state.openReject}
+                            onClose={this.handleClose}
+                            className="right-dir font-applied"
+                        >
+                            <DialogTitle id="alert-dialog-title" className="font-applied"> <Icon style={{color: 'red'}}
+                                                                                                 fontSize="large">close_circle</Icon>{"تور مورد نظر رد شد"}
+                            </DialogTitle>
+                            <DialogContent>
+                                <DialogContentText id="alert-dialog-description">
+                                    پشتیبان گرامی! عملیات عدم تایید تور مورد نظر انجام شد.بر روی کلید زیر کلیک کنید تا
+                                    به لیست تور ها منتقل شوید.
+                                </DialogContentText>
+                            </DialogContent>
+                            <DialogActions>
+                                <Button component={NavLink} to="/admin-tours-list" variant="contained" className="font-applied" onClick={this.handleClose} color="primary" autoFocus>
+                                    لیست تورها
+                                </Button>
+                            </DialogActions>
+                        </Dialog>
+                        <Dialog
+                            open={this.state.openAccept}
+                            onClose={this.handleClose}
+                            className="right-dir font-applied"
+                        >
+                            <DialogTitle id="alert-dialog-title" className="font-applied"> <Icon
+                                style={{color: 'green'}}
+                                fontSize="large">check_circle</Icon>{"انجام شد!"}
+                            </DialogTitle>
+                            <DialogContent>
+                                <DialogContentText id="alert-dialog-description">
+                                    پشتیبان گرامی! عملیات تایید تور مورد نظر انجام شد.بر روی کلید زیر کلیک کنید تا
+                                    به لیست تور ها منتقل شوید.
+                                </DialogContentText>
+                            </DialogContent>
+                            <DialogActions>
+                                <Button component={NavLink} to="/admin-tours-list"  variant="contained" className="font-applied" onClick={this.handleClose} color="primary" autoFocus>
+                                    لیست تورها
+                                </Button>
+                            </DialogActions>
+                        </Dialog>
                     </Paper>
                 </Grid>
             </Grid>
@@ -54,7 +118,7 @@ class TourDetail extends React.Component {
 }
 
 const mapStateToProps = (state) => ({
-    tours:state.tours
+    tours: state.tours
 });
 
 export default connect(mapStateToProps)(TourDetail);
