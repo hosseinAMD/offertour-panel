3053d9956871a277c0b1a3611f52b36c386ddc3a
Hossein Ahmadi <hossein.ahmadi98@outlook.com>
2019-04-26 18:28:40 +0430

    phone number display done

--- src/components/AddArticle.js
+++ src/components/AddArticle.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Divider from '@material-ui/core/Divider';
+import HeaderChip from './HeaderChip';
 import ArticleForm from "./ArticleForm";
 
 const AddArticle = () => (
     <Grid container spacing={24} className="my-container">
         <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-            <ArticleForm/>
+            <Paper elevation={1} className="right-dir agency-paper">
+                <HeaderChip label='افزودن سفرنامه' color='#0288d1' icon='edit'/>
+                <Divider/>
+                <br/>
+                <ArticleForm/>
+            </Paper>
         </Grid>
     </Grid>
 );
--- src/components/ArticleForm.js
+++ src/components/ArticleForm.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import Paper from '@material-ui/core/Paper';
-import CKEditor from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import Icon from "@material-ui/core/Icon";
+import FormControl from '@material-ui/core/FormControl';
+import Button from '@material-ui/core/Button';
+import {NavLink} from "react-router-dom";
 
-class ArticleForm extends React.Component{
-    constructor(props){
+class ArticleForm extends React.Component {
+    constructor(props) {
         super(props);
         this.state = {
-            title:'',
-            text:''
+            title: '',
+            text: '',
+            image: '',
+            tag: '',
+            tags: []
         }
     }
 
@@ -17,33 +22,69 @@ class ArticleForm extends React.Component{
         this.setState({[name]: event.target.value});
     };
 
+    imageChange = (e) => {
+        const image = e.target.files[0];
+        this.setState(() => ({image}));
+    };
+
+    addTag = () => {
+        const tag = this.state.tag;
+        let tags = this.state.tags;
+        tags.push(tag);
+        this.setState(() => ({tags, tag: ''}));
+    };
+
+
     render() {
-        return(
+        return (
             <Paper elevation={1} className="article-form-paper right-dir">
-                <h3 className="font-applied">ارسال سفرنامه</h3>
-                <TextField
-                    id="title"
-                    label="عنوان"
-                    InputLabelProps={{className: 'input-labels'}}
-                    InputProps={{className: 'font-applied'}}
-                    FormHelperTextProps={{className: 'font-applied'}}
-                    helperText='لطفا عنوانی مرتبط و گویا برای مقاله خود انتخاب کنید.'
-                    value={this.state.title}
-                    onChange={this.handleChange('title')}
-                    margin="normal"
-                />
-                <CKEditor
-                    editor={ ClassicEditor }
-                    data={this.state.text}
-                    congif={{
-                        alignment:'right'
-                    }}
-                    onChange={ ( event, editor ) => {
-                        const data = editor.getData();
-                        this.setState(() => ({text:data}))
-                    } }
-                />
-                <div dangerouslySetInnerHTML={{__html:this.state.text}}></div>
+                <div className="article-form-wrapper">
+                    <TextField
+                        id="title"
+                        label="عنوان"
+                        autoFocus={true}
+                        InputLabelProps={{className: 'input-labels'}}
+                        InputProps={{className: 'font-applied'}}
+                        FormHelperTextProps={{className: 'font-applied'}}
+                        helperText='لطفا عنوانی مرتبط و گویا برای مقاله خود انتخاب کنید.'
+                        value={this.state.title}
+                        onChange={this.handleChange('title')}
+                        margin="normal"
+                    />
+                    <TextField
+                        id="text"
+                        label="متن"
+                        multiline
+                        rows={8}
+                        InputLabelProps={{className: 'input-labels'}}
+                        InputProps={{className: 'font-applied'}}
+                        value={this.state.text}
+                        onChange={this.handleChange('text')}
+                        margin="normal"
+                    />
+                    <div style={{display: 'flex', flexFlow: 'wrap', justifyContent: 'space-around'}}>
+                        <div>
+                            <TextField
+                                id="tag"
+                                label="برچسب"
+                                InputLabelProps={{className: 'input-labels'}}
+                                InputProps={{className: 'font-applied'}}
+                                value={this.state.tag}
+                                onChange={this.handleChange('tag')}
+                                margin="normal"
+                            />
+                            <Icon fontSize="large" onClick={this.addTag}
+                                  style={{color: 'blue', cursor: 'pointer'}}>add</Icon>
+                        </div>
+                        <FormControl>
+                            <label style={{textAlign: 'right', margin: '5px'}} htmlFor="tour-image">تصویر مقاله</label>
+                            <input id="tour-image" type="file" onChange={this.imageChange}/>
+                        </FormControl>
+                    </div>
+                    <p>{this.state.tags.map((tag) => <span className="tag">{tag}</span>)}</p>
+                    <Button variant="contained" color="primary"
+                            className="edit-button">ارسال</Button>
+                </div>
             </Paper>
         );
     }
