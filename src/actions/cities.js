8f1663b1d6de95e9c6d27d35042f56d897a33eec
Hossein Ahmadi <hossein.ahmadi98@outlook.com>
2019-04-26 12:15:11 +0430

    add city action and reducer done

--- src/actions/cities.js
+++ src/actions/cities.js
@@ -4,3 +4,8 @@ export const setCities = (citiesArray) => ({
     type: 'SET_CITY',
     citiesArray
 });
+
+export const addCity = (cityObject) => ({
+    type: 'ADD_CITY',
+    cityObject
+});
--- src/reducers/cities.js
+++ src/reducers/cities.js
@@ -6,6 +6,8 @@ const cityReducer = (state = cityReducerDefaultState, action) => {
     if (action.type === 'SET_CITY') {
         state = action.citiesArray;
         return state;
+    } else if (action.type === 'ADD_CITY') {
+        return [...state, action.cityObject];
     } else {
         return state;
     }
