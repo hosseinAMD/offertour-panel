5d7a3c8cc0e03b4fab5adb2d1dc811a7d33bf264
Hossein Ahmadi <hossein.ahmadi98@outlook.com>
2019-04-26 12:10:55 +0430

    add agency action and reducer done

--- src/actions/agencies.js
+++ src/actions/agencies.js
@@ -4,3 +4,8 @@ export const setAgencies = (agenciesArray) => ({
     type: 'SET_AGENCY',
     agenciesArray
 });
+
+export const addAgency = (agencyObject) => ({
+    type: 'ADD_AGENCY',
+    agencyObject
+});
--- src/reducers/agencies.js
+++ src/reducers/agencies.js
@@ -6,6 +6,8 @@ const agencyReducer = (state = agencyReducerDefaultState, action) => {
     if (action.type === 'SET_AGENCY') {
         state = action.agenciesArray;
         return state;
+    } else if (action.type === 'ADD_AGENCY') {
+        return [...state, action.agencyObject];
     } else {
         return state;
     }
