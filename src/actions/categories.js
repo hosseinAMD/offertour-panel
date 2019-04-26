30fa7d3b4083b4d0600b23762274efc999402850
Hossein Ahmadi <hossein.ahmadi98@outlook.com>
2019-04-26 12:13:54 +0430

    add category action and reducer done

--- src/actions/categories.js
+++ src/actions/categories.js
@@ -4,3 +4,8 @@ export const setCategories = (categoriesArray) => ({
     type: 'SET_CATEGORY',
     categoriesArray
 });
+
+export const addCategory = (categoryObject) => ({
+    type: 'ADD_CATEGORY',
+    categoryObject
+});
--- src/reducers/categories.js
+++ src/reducers/categories.js
@@ -6,6 +6,8 @@ const categoryReducer = (state = categoryReducerDefaultState, action) => {
     if (action.type === 'SET_CATEGORY') {
         state = action.categoriesArray;
         return state;
+    } else if (action.type === 'ADD_CATEGORY') {
+        return [...state, action.categoryObject];
     } else {
         return state;
     }
