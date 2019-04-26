8602c0a92e835be43378c4f15561d46733bcec00
Hossein Ahmadi <hossein.ahmadi98@outlook.com>
2019-04-26 15:27:49 +0430

    tour detail dates bug fixed

--- src/components/TourInfo.js
+++ src/components/TourInfo.js
@@ -6,44 +6,45 @@ import TableRow from '@material-ui/core/TableRow';
 import numeral from "numeral";
 import ItemRenderer from './ItemRenderer';
 import moment from "moment-jalaali";
+import enmoment from "moment";
 
-const TourInfo = (props) => (
-    <div className="font-applied right-dir tour-info">
-        <Table className="font-applied right-dir">
-            <TableBody>
-                <TableRow>
-                    <TableCell align="center"><span className="bold">عنوان تور</span></TableCell>
-                    <TableCell align="center">{props.tour.Title}</TableCell>
-                </TableRow>
-                <TableRow>
-                    <TableCell align="center"><span className="bold">شروع قیمت</span></TableCell>
-                    <TableCell align="center">{numeral(props.tour.StartPrice).format('0,0')}</TableCell>
-                </TableRow>
-                <TableRow>
-                    <TableCell align="center"><span className="bold">مدت تور</span></TableCell>
-                    <TableCell align="center">{props.tour.Duration}</TableCell>
-                </TableRow>
-                <TableRow>
-                    <TableCell align="center"><span className="bold">شهر</span></TableCell>
-                    <TableCell align="center"><ItemRenderer id={props.tour.CityID}
-                                                            type="city"/></TableCell>
-                </TableRow>
-                <TableRow>
-                    <TableCell align="center"><span className="bold">هواپیمایی</span></TableCell>
-                    <TableCell align="center"><ItemRenderer
-                        id={props.tour.TourDistance} type="flightCompany"/></TableCell>
-                </TableRow>
-                <TableRow>
-                    <TableCell align="center"><span className="bold">تاریخ شروع</span></TableCell>
-                    <TableCell align="center">{moment(parseInt(props.tour.TourDate)).format('jYYYY/jMM/jDD')}</TableCell>
-                </TableRow>
-                <TableRow>
-                    <TableCell align="center"><span className="bold">تاریخ پایان</span></TableCell>
-                    <TableCell align="center">{moment(props.tour.TourEndDate).format('jYYYY/jMM/jDD')}</TableCell>
-                </TableRow>
-            </TableBody>
-        </Table>
-    </div>
-);
+const TourInfo = (props) => {
+    const tourDate = enmoment.unix(Number(props.tour.TourDate)).format('YYYY/MM/DD');
+    const tourEndDate = enmoment.unix(Number(props.tour.TourReturnDate)).format('YYYY/MM/DD');
+    return (
+        <div className="font-applied right-dir tour-info">
+            <Table className="font-applied right-dir">
+                <TableBody>
+                    <TableRow>
+                        <TableCell align="center"><span className="bold">عنوان تور</span></TableCell>
+                        <TableCell align="center">{props.tour.Title}</TableCell>
+                    </TableRow>
+                    <TableRow>
+                        <TableCell align="center"><span className="bold">شروع قیمت</span></TableCell>
+                        <TableCell align="center">{numeral(props.tour.StartPrice).format('0,0')}</TableCell>
+                    </TableRow>
+                    <TableRow>
+                        <TableCell align="center"><span className="bold">مدت تور</span></TableCell>
+                        <TableCell align="center">{props.tour.Duration}</TableCell>
+                    </TableRow>
+                    <TableRow>
+                        <TableCell align="center"><span className="bold">شهر</span></TableCell>
+                        <TableCell align="center"><ItemRenderer id={props.tour.CityID}
+                                                                type="city"/></TableCell>
+                    </TableRow>
+                    <TableRow>
+                        <TableCell align="center"><span className="bold">تاریخ شروع</span></TableCell>
+                        <TableCell
+                            align="center">{moment(tourDate).format('jYYYY/jMM/jDD')}</TableCell>
+                    </TableRow>
+                    <TableRow>
+                        <TableCell align="center"><span className="bold">تاریخ پایان</span></TableCell>
+                        <TableCell align="center">{moment(tourEndDate).format('jYYYY/jMM/jDD')}</TableCell>
+                    </TableRow>
+                </TableBody>
+            </Table>
+        </div>
+    );
+}
 
 export default TourInfo;
