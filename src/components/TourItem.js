3dea6e892c68d740fd0a27b7b418dca9f52eeef6
Hossein Ahmadi <hossein.ahmadi98@outlook.com>
2019-04-26 15:23:44 +0430

    tours dates bug fixed

--- src/components/TourItem.js
+++ src/components/TourItem.js
@@ -4,23 +4,28 @@ import TableRow from '@material-ui/core/TableRow';
 import ItemRenderer from "./ItemRenderer";
 import numeral from 'numeral';
 import moment from 'moment-jalaali';
+import enmoment from 'moment';
 import StatusRenderer from "./StatusRenderer";
 import FeaturedStatusRenderer from "./FeaturedStatusRenderer";
 import {NavLink} from "react-router-dom";
 
-const TourItem = (props) => (
-    <TableRow>
-        <TableCell align="center">{props.tour.Id}</TableCell>
-        <TableCell component={NavLink} to={`/tour/${props.tour.Id}`} align="center"><span
-            className="bold">{props.tour.Title}</span></TableCell>
-        <TableCell align="center"><ItemRenderer id={props.tour.TourCityID} type="city"/></TableCell>
-        <TableCell align="center">{numeral(props.tour.StartPrice).format('0,0')}</TableCell>
-        <TableCell align="center">{moment(parseInt(props.tour.TourDate)).format('jYYYY/jMM/jDD')}</TableCell>
-        <TableCell align="center">{moment(parseInt(props.tour.TourReturnDate)).format('jYYYY/jMM/jDD')}</TableCell>
-        <TableCell align="center">{props.tour.Duration}</TableCell>
-        <TableCell align="center"><StatusRenderer status={props.tour.Status}/></TableCell>
-        <TableCell align="center"><FeaturedStatusRenderer featured={props.tour.TourModelID}/></TableCell>
-    </TableRow>
-);
+const TourItem = (props) => {
+        const tourDate = enmoment.unix(Number(props.tour.TourDate)).format('YYYY/MM/DD');
+        const tourEndDate = enmoment.unix(Number(props.tour.TourReturnDate)).format('YYYY/MM/DD');
+        return(
+            <TableRow>
+                    <TableCell align="center">{props.tour.Id}</TableCell>
+                    <TableCell component={NavLink} to={`/tour/${props.tour.Id}`} align="center"><span
+                        className="bold">{props.tour.Title}</span></TableCell>
+                    <TableCell align="center"><ItemRenderer id={props.tour.TourCityID} type="city"/></TableCell>
+                    <TableCell align="center">{numeral(props.tour.StartPrice).format('0,0')}</TableCell>
+                    <TableCell align="center">{moment(tourDate).format('jYYYY/jMM/jDD')}</TableCell>
+                    <TableCell align="center">{moment(tourEndDate).format('jYYYY/jMM/jDD')}</TableCell>
+                    <TableCell align="center">{props.tour.Duration}</TableCell>
+                    <TableCell align="center"><StatusRenderer status={props.tour.Status}/></TableCell>
+                    <TableCell align="center"><FeaturedStatusRenderer featured={props.tour.TourModelID}/></TableCell>
+            </TableRow>
+        );
+}
 
 export default TourItem;
