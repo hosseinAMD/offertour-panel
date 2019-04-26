d7277242724e61be0f03f5f0147ea287307b0644
Hossein Ahmadi <hossein.ahmadi98@outlook.com>
2019-04-26 12:12:25 +0430

    add airport action and reducer done

--- src/actions/airports.js
+++ src/actions/airports.js
@@ -4,3 +4,8 @@ export const setAirports = (airportsArray) => ({
     type: 'SET_AIRPORT',
     airportsArray
 });
+
+export const addAirport = (airportObject) => ({
+    type: 'ADD_AIRPORT',
+    airportObject
+});
--- src/reducers/airports.js
+++ src/reducers/airports.js
@@ -6,6 +6,8 @@ const airportReducer = (state = airportReducerDefaultState, action) => {
     if (action.type === 'SET_AIRPORT') {
         state = action.airportsArray;
         return state;
+    } else if (action.type === 'ADD_AIRPORT') {
+        return [...state, action.airportObject];
     } else {
         return state;
     }
