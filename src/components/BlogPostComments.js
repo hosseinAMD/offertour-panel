d13388a94e198b89c64266199ebfae9bf37d09e1
Hossein Ahmadi <hossein.ahmadi98@outlook.com>
2019-05-05 13:50:33 +0430

    blog post comment accept and reject done

--- src/components/BlogPostComments.js
+++ src/components/BlogPostComments.js
@@ -3,7 +3,8 @@ import Paper from '@material-ui/core/Paper';
 import HeaderChip from './HeaderChip';
 import Divider from "@material-ui/core/Divider";
 import axios from 'axios';
-import baseUrl from '../config/config';
+import baseUrl, {token} from '../config/config';
+import {statusCodes} from "../config/errors";
 import Loading from "./Loading";
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -11,11 +12,44 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import PostCommentItem from "./PostCommentItem";
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Icon from "@material-ui/core/Icon";
+import Button from '@material-ui/core/Button';
 
 class BlogPostComments extends React.Component {
     state = {
         comments: '',
-        isLoaded: false
+        isLoaded: false,
+        openLoading: false,
+        openSuccess: false,
+        openError: false,
+        error: ''
+    };
+
+    handleClose = () => {
+        this.setState({openSuccess: false, openLoading: false, openError: false});
+    };
+
+    handleAccept = (status, id) => {
+        this.setState(() => ({openLoading: true}));
+        axios.put(baseUrl + '/Admin/Blog', JSON.stringify({
+            BlogID: id,
+            Status: !status
+        }), {
+            headers: {
+                'Content-Type': 'application/json',
+                'token': token
+            }
+        }).then(res => this.setState(() => ({openSuccess: true, openLoading: false})))
+            .catch(res => this.setState(() => ({
+                openLoading: false,
+                error: statusCodes.FA[res.response.data.code].message,
+                openError: true
+            })));
     };
 
     componentDidMount() {
@@ -50,9 +84,61 @@ class BlogPostComments extends React.Component {
                                 </TableRow>
                             </TableHead>
                             <TableBody>
-                                {this.state.comments.map((comment,index) => <PostCommentItem comment={comment} index={index}/>)}
+                                {this.state.comments.map((comment, index) => <PostCommentItem
+                                    handleAccept={this.handleAccept} comment={comment}
+                                    index={index}/>)}
                             </TableBody>
                         </Table>
+                        <Dialog
+                            open={this.state.openSuccess}
+                            onClose={this.handleClose}
+                            className="right-dir font-applied"
+                        >
+                            <DialogTitle id="alert-dialog-title" className="font-applied"> <Icon
+                                style={{color: 'green'}}
+                                fontSize="large">check_circle</Icon>{"انجام شد!"}
+                            </DialogTitle>
+                            <DialogContent>
+                                <DialogContentText id="alert-dialog-description">
+                                    تغییر وضعیت نظر با موفقیت انجام شد!
+                                </DialogContentText>
+                            </DialogContent>
+                            <DialogActions>
+                                <Button className="font-applied" onClick={this.handleClose} color="primary" autoFocus>
+                                    بستن
+                                </Button>
+                            </DialogActions>
+                        </Dialog>
+                        <Dialog
+                            open={this.state.openError}
+                            onClose={this.handleClose}
+                            className="right-dir font-applied"
+                        >
+                            <DialogTitle id="alert-dialog-title" className="font-applied"> <Icon style={{color: 'red'}}
+                                                                                                 fontSize="large">close_circle</Icon>{"خطا در بروزرسانی اطلاعات"}
+                            </DialogTitle>
+                            <DialogContent>
+                                <DialogContentText id="alert-dialog-description">
+                                    {this.state.error}
+                                </DialogContentText>
+                            </DialogContent>
+                            <DialogActions>
+                                <Button className="font-applied" onClick={this.handleClose} color="primary" autoFocus>
+                                    بستن
+                                </Button>
+                            </DialogActions>
+                        </Dialog>
+                        <Dialog
+                            open={this.state.openLoading}
+                            onClose={this.handleClose}
+                            className="right-dir font-applied"
+                        >
+                            <DialogTitle id="alert-dialog-title" className="font-applied">{"در حال انجام عملیات!"}
+                            </DialogTitle>
+                            <DialogContent>
+                                <Loading/>
+                            </DialogContent>
+                        </Dialog>
                     </Paper>
                 );
             } else {
--- src/components/PostCommentItem.js
+++ src/components/PostCommentItem.js
@@ -4,18 +4,25 @@ import TableCell from '@material-ui/core/TableCell';
 import enmoment from "moment";
 import moment from 'moment-jalaali';
 import PlanStars from "./PlanStars";
+import StatusRenderer from "./StatusRenderer";
+import Button from "@material-ui/core/Button";
 
 
 const PostCommentItem = (props) => {
     const PostageDateTime = enmoment(Number(props.comment.PostageDateTime)).format('YYYY/MM/DD');
+    const status = props.comment.Status;
+    const id = props.comment.Id;
     return (
         <TableRow>
             <TableCell align="center">{props.index + 1}</TableCell>
             <TableCell align="center">{props.comment.CommentText}</TableCell>
             <TableCell align="center">{moment(PostageDateTime).format('jDD jMMMM jYYYY')}</TableCell>
             <TableCell align="center">{<PlanStars star={props.comment.PointOFPost}/>}</TableCell>
-            <TableCell align="center">{props.comment.Status}</TableCell>
-            <TableCell align="center">sss</TableCell>
+            <TableCell align="center"><StatusRenderer status={props.comment.Status}/></TableCell>
+            <TableCell align="center"><Button onClick={() => props.handleAccept(status, id)}
+                                              variant="contained"
+                                              color={props.comment.Status ? 'secondary' : 'primary'}
+                                              className="font-applied">{props.comment.Status ? 'غیرفعال' : 'فعال'}</Button></TableCell>
         </TableRow>
     );
 };
